468a4df2e7eef6b71a8f6195ae900018adc82b7c
ManashRaja <mpdmanash@gmail.com>
2016-06-27 13:55:45 +0530

    Working Face and People Detection

---
+++ detect_people.py
@@ -0,0 +1,55 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import imutils
+import cv2
+
+
+class DetectPeople(object):
+    _hog = None
+
+    def __init__(self):
+        pass
+
+    def detect(self, imgs):
+        """ initialize the HOG descriptor/person detector
+        # load the image and resize it to (1) reduce detection time
+        # and (2) improve detection accuracy """
+
+        _image_num = -1
+        _max_detections = 0
+        for i in range(0, 2):
+            image = imgs[i]
+            image = image[25:image.shape[2] - 25, 0:image.shape[1]]
+            image = imutils.resize(image, width=min(400, image.shape[1]))
+            # orig = image.copy()
+
+            # detect people in the image
+            _hog = cv2.HOGDescriptor()
+            _hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+            (rects, weights) = _hog.detectMultiScale(image, winStride=(4, 4),
+                                                     padding=(8, 8), scale=1.05)
+
+            # draw the original bounding boxes
+            # for (x, y, w, h) in rects:
+            #     cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+            # apply non-maxima suppression to the bounding boxes using a
+            # fairly large overlap threshold to try to maintain overlapping
+            # boxes that are still people
+            rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+            pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+            # draw the final bounding boxes
+            if len(pick) > _max_detections:
+                _max_detections = len(pick)
+                _image_num = i
+                for (xA, yA, xB, yB) in pick:
+                    cv2.rectangle(imgs[i], (xA, yA), (xB, yB), (255, 0, 0), 2)
+
+            # show the output images
+            # cv2.imshow("Before NMS", orig)
+            # cv2.imshow("After NMS", image)
+            # cv2.waitKey(0)
+            return _image_num
---
+++ smtp_server.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+import asyncore
+from smtpd import SMTPServer
+import re
+from PIL import Image
+from io import BytesIO
+import base64
+import cv2
+import numpy as np
+from detect_people import DetectPeople
+
+
+class EmlServer(SMTPServer):
+    no = 0
+    raw = 0
+    dp = DetectPeople()
+
+    def readb64(self, base64_string):
+        im = Image.open(BytesIO(base64.b64decode(base64_string)))
+        return cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+
+    def decode_imges(self, data):
+        data = data.replace('\n', '')
+        images = re.findall("base64([^#]+)--#BOUNDARY", data)
+        img1 = self.readb64(images[0])
+        img2 = self.readb64(images[1])
+        img3 = self.readb64(images[2])
+        filename = 'images/raw/%s-%d.jpg' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.raw)
+        cv2.imwrite(filename, img1)
+        self.raw += 1
+        filename = 'images/raw/%s-%d.jpg' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.raw)
+        cv2.imwrite(filename, img2)
+        self.raw += 1
+        filename = 'images/raw/%s-%d.jpg' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.raw)
+        cv2.imwrite(filename, img3)
+        self.raw += 1
+        return (img1, img2, img3)
+
+    def detect_faces(self, imgs):
+        image_no = -1
+        max_detections = 0
+        faceCascade = cv2.CascadeClassifier("ml_trained/haarcascade_frontalface_alt.xml")
+        for i in range(0, 2):
+            gray = cv2.cvtColor(imgs[i], cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+            if len(faces) > max_detections:
+                max_detections = len(faces)
+                image_no = i
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(imgs[i], (x, y), (x + w, y + h), (0, 255, 0), 2)
+        return image_no
+
+    def write_image(self, image_no, imgs):
+        if image_no != -1:
+            filename = 'images/%s-%d.jpg' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.no)
+            cv2.imwrite(filename, imgs[image_no])
+            self.no += 1
+
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        print "Received Email"
+        # Decode images
+        imgs = []
+        (img1, img2, img3) = self.decode_imges(data)
+        imgs.append(img1)
+        imgs.append(img2)
+        imgs.append(img3)
+
+        # Detect faces in images
+        image_no = self.detect_faces(imgs)
+        self.write_image(image_no, imgs)
+
+        # Detect People
+        image_no = self.dp.detect(imgs)
+        self.write_image(image_no, imgs)
+
+
+def run():
+    print "Running"
+    server = EmlServer(('192.168.0.100', 587), None)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        pass
+
+
+run()
