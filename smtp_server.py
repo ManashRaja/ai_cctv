5655067615290554220733bb536aadf2074cba34
ManashRaja <mpdmanash@gmail.com>
2016-06-28 21:52:46 +0530

    Fixed: Issue #1. Image mask

--- smtp_server.py
+++ smtp_server.py
@@ -1,8 +1,10 @@
+import os
 import re
 import cv2
 import base64
 import asyncore
 import numpy as np
+import ConfigParser
 import multi_threading
 from time import time
 from PIL import Image
@@ -14,11 +16,14 @@ from detect_people import DetectPeople
 
 
 class EmlServer(SMTPServer):
-    no = 0
-    raw = 0
-    dp = DetectPeople()
-    data_queue = Queue()
-    img_queue = Queue()
+    def __init__(self, local_address, remote_address, config):
+        SMTPServer.__init__(self, local_address, remote_address)
+        self.no = 0
+        self.raw = 0
+        self.dp = DetectPeople()
+        self.data_queue = Queue()
+        self.img_queue = Queue()
+        self.config = config
 
     def readb64(self, base64_string):
         im = Image.open(BytesIO(base64.b64decode(base64_string)))
@@ -26,13 +31,27 @@ class EmlServer(SMTPServer):
 
     def decode_imges(self, data):
         data = data.replace('\n', '')
+        camera_regex = self.config.get('Settings', 'Camera Regex')
+        found = re.search(camera_regex, data)
+        camera_name = "None"
+        if found:
+            camera_name = found.group(1)
+            print camera_name
         image_strings = re.findall("base64([^#]+)--#BOUNDARY", data)
         imgs = []
         imgs.append(self.readb64(image_strings[0]))
         imgs.append(self.readb64(image_strings[1]))
         imgs.append(self.readb64(image_strings[2]))
 
+        mask = None
+        mask_path = "settings/masks/" + camera_name + ".jpg"
+        print mask_path
+        if os.path.isfile(mask_path):
+            mask = cv2.imread(mask_path)
+
         for i in range(3):
+            if mask is not None:
+                imgs[i] = cv2.add(imgs[i], mask)
             filename = 'images/raw/%s-%d.jpg' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.raw)
             cv2.imwrite(filename, imgs[i])
             self.raw += 1
@@ -47,7 +66,7 @@ class EmlServer(SMTPServer):
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE # previously cv2.cv.CV_HAAR_SCALE_IMAGE
+            flags=cv2.CASCADE_SCALE_IMAGE  # previously cv2.cv.CV_HAAR_SCALE_IMAGE
         )
         if len(faces) > 0:
             # Draw a rectangle around the faces
@@ -68,10 +87,11 @@ class EmlServer(SMTPServer):
         self.data_queue.put(data)
         print('Took {}'.format(time() - ts))
 
-
 def run():
     print "Running"
-    server = EmlServer(('0.0.0.0', 587), None)
+    config = ConfigParser.ConfigParser()
+    config.read("settings/config.ini")
+    server = EmlServer(('0.0.0.0', 587), None, config)
     for x in range(1):
         data_worker = multi_threading.DataWorker(server)
         # Setting daemon to True will let the main thread exit even though the
